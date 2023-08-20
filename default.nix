36bcd6bba59f5d2994d8492afaf539ca9f09b655
creator54 <hi.creator54@gmail.com>
2023-08-21 01:55:41 +0530

    setup in nix -       magic is not getting imported via -       requirements.txt on nix-shell hence specified via nix

---
+++ default.nix
@@ -0,0 +1,64 @@
+with import <nixpkgs> { };
+pkgs.mkShell rec {
+  name = "impurePythonEnv";
+  venvDir = "./.venv";
+  buildInputs = [
+    python310
+    python310Packages.python-magic
+  ];
+
+  shellHook = ''
+    set -h #remove "bash: hash: hashing disabled" warning !
+    SOURCE_DATE_EPOCH=$(date +%s)
+
+    export LD_LIBRARY_PATH="${lib.makeLibraryPath [ zlib stdenv.cc.cc ]}":LD_LIBRARY_PATH;
+
+    if ! [ -d "${venvDir}" ]; then
+      python -m venv "${venvDir}"
+    fi
+    source "${venvDir}/bin/activate"
+    python -m pip install --upgrade pip
+    pip install -r requirements.txt
+    eval "$extras"
+    export FLASK_APP=fhost
+  '';
+  extras = ''
+    pymod() {
+    	pip list
+    }
+
+    pyadd() {
+    	for pkg in "$@"; do
+    		if ! grep -q "$pkg" requirements.txt; then
+    			if pip install "$pkg"; then
+    				echo "$pkg" >>requirements.txt
+          fi
+    		fi
+    	done
+    }
+
+    pyrm() {
+    	if [ $# -eq 0 ] && [ -e ./requirements.txt ] && [ -s ./requirements.txt ]; then
+    		pkg=$(cat requirements.txt | fzf)
+    		if [ -n "$pkg" ]; then
+    			grep -v "$pkg" requirements.txt >requirements.tmp
+    			mv requirements.tmp requirements.txt
+    			pip uninstall "$pkg" -y
+    		fi
+    	else
+    		for pkg in "$@"; do
+    			grep -v "$pkg" requirements.txt >requirements.tmp
+    			mv requirements.tmp requirements.txt
+    			pip uninstall "$pkg" -y
+    		done
+    	fi
+    }
+
+    if [ -n "$IN_NIX_SHELL" ]; then
+    	echo "Extra Functions:"
+    	echo "pymod : Show a list of installed Python packages."
+    	echo "pyadd : Add packages to requirements.txt and install them."
+    	echo "pyrm  : Remove packages from requirements.txt and uninstall them."
+    fi
+  '';
+}
